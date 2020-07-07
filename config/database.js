e1feaed2b13d5aac9856fc2a18799babb171c357
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-07-07 15:33:20 +0800

    Deploy app to Google App Engine

--- config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(mongoose) {
-  mongoose.connect('mongodb://localhost:27017/authPassportTest', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(function() {
-      console.log('Connected to MongoDB...')
-    });
-};
